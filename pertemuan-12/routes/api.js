bf793dcaff54cc0cb82ad129f9da19d01f95e38f
rakhazan <rakhazahnur26@gmail.com>
2022-12-19 20:20:05 +0700

    delete dotenv file

--- pertemuan-12/routes/api.js
+++ pertemuan-12/routes/api.js
@@ -18,6 +18,7 @@ router.get('/', (req, res) => {
 
 router.get('/students', StudentController.index)
 router.post('/students', StudentController.store)
+router.get('/students/:id', StudentController.show)
 // router.put('/students/:id', StudentController.update)
 // router.delete('/students/:id', StudentController.destroy)
 
